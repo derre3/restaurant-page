4e6d5d785c6a4e28cea7c6ce24273b3fa86e4191
Andre <123985988+derre3@users.noreply.github.com>
2023-08-14 18:19:19 -0300

    update about page

--- src/about.js
+++ src/about.js
@@ -4,21 +4,24 @@ export default function aboutComponent() {
   const mainContent = document.createElement("div");
   mainContent.classList.add("main-content");
   const logoImage = new Image();
-  const para1 = document.createElement("p");
-  const para2 = document.createElement("p");
+  const para1 = document.createElement("h1");
+  const para2 = document.createElement("h3");
+  const para3 = document.createElement("h3");
   const paraContainer = document.createElement("div");
   const aboutContainer = document.createElement("div");
   aboutContainer.classList.add("about-container");
-  para1.textContent =
-    "Temos como objetivo levar mais sabor para o seu café da manhã ou da tarde.";
-  para2.textContent =
-    "Nossos pães são artesanais, feitos sem conservantes, com produtos de qualidade prezando o sabor.";
+  para1.textContent = "GS Pães©";
+  para2.textContent = "Trazendo sabor para o seu café da manhã";
+  para3.textContent =
+    "Pães artesanais, feitos sem conservantes, com produtos de qualidade prezando o sabor.";
   logoImage.src = Logo;
   logoImage.id = "logo-image";
   paraContainer.appendChild(para1);
   paraContainer.appendChild(para2);
-  aboutContainer.appendChild(logoImage);
+  // paraContainer.appendChild(para3);
   aboutContainer.appendChild(paraContainer);
+  aboutContainer.appendChild(logoImage);
+  aboutContainer.appendChild(para3);
   mainContent.appendChild(aboutContainer);
   return mainContent;
 }
